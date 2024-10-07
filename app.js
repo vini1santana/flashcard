b83d2a8f76e3692e6e8f1b429bd7e71504a4e50b
vinicius <163008525+vini1santana@users.noreply.github.com>
2024-10-07 11:32:52 -0300

    .

---
+++ app.js
@@ -0,0 +1,16 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao'; 
+}
+
+cartao.innerHTML = 
+<div class="cartao_conteudo">
+            <h3>programação</h3>
+            <div class="cartao__conteudo__pergunta">
+                <p>O que é javaScript</p>
+            </div>
+            <div class="cartao__conteudo__resposta">
+                <p>javaScript é uma linguagem de programação</p>
+            </div>
+        </div>
---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'programação 2.0',
+    'o que é phynton?',
+    'o phynton é uma linguagem de progamação'
+)
+criaCartao(
+    'futebol 2.0',
+    'Em que ano o Brasil ganhou seu último titulo mundial?',
+    'o último titulo do Brasil foi em 2002'
+)
+criaCartao(
+    'esporte 2.0',
+    'Quem é considerado o maior atleta de todos os tempos no esporte?',
+    'o maior atleta considerado é o  Michael phelps'
+)
+criaCartao(
+    'partes do corpo 2.0',
+    'Quantos ossos tem no corpo humano?',
+    'são 206 ossos pelo corpo humano'
+)
+criaCartao(
+    'partes do corpo 2.0',
+    'Qual o maior órgão do corpo humano?',
+    'o maior órgão do corpo humano é a pele'
+)
+criaCartao(
+    'esporte 2.0',
+    'Quem ganhou a Copa do Mundo de 2010?',
+    'foi a Espanha'
+)
+criaCartao(
+    'conhecimento geral 2.0',
+    'Qual país é mais rico?',
+    'Luxemburgo'
+)
